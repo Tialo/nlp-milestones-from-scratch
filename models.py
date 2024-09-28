9115b0d375ff8b0b68d6232902dd57ba99b8de22
Tialo <tialox@yandex.ru>
2024-09-28 15:05:29 +0300

    add EncoderBlock

--- models.py
+++ models.py
@@ -30,7 +30,7 @@ class AttentionBlock(nn.Module):
         q = self.q_weights(x)
         k = self.k_weights(x)
         v = self.v_weights(x)
-        return self.attention_function(q, k, v)
+        return self.attention_function(q, k, v)  # (batch_size, seq_len, d_v)
 
 
 class MultiHeadAttentionBlock(nn.Module):
@@ -48,5 +48,31 @@ class MultiHeadAttentionBlock(nn.Module):
         attentions = [  # (n_heads, batch_size, seq_len, d_v)
             attention_block(x) for attention_block in self.attention_blocks
         ]
-        attentions = torch.concat(attentions).reshape(batch_size, seq_len, -1)
-        return self.concat_head(attentions)
+        attentions = torch.concat(attentions).reshape(batch_size, seq_len, -1)  # (batch_size, seq_len, n_heads * d_v)
+        return self.concat_head(attentions)  # (batch_size, seq_len, d_out)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_out, d_ff):
+        super().__init__()
+        self.ff = nn.Sequential(
+            nn.Linear(d_out, d_ff),
+            nn.ReLU(),
+            nn.Linear(d_ff, d_out)
+        )
+
+    def forward(self, x):
+        return self.ff(x)
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, n_heads, d_x, d_qk, d_v, d_out, d_ff):
+        super().__init__()
+        self.ff = FeedForward(d_out, d_ff)
+        self.mha = MultiHeadAttentionBlock(n_heads, d_x, d_qk, d_v, d_out)
+        self.ln1 = nn.LayerNorm(d_out)
+        self.ln2 = nn.LayerNorm(d_out)
+
+    def forward(self, x):
+        attention = self.ln1(self.mha(x) + x)  # (batch_size, seq_len, d_out)
+        return self.ln2(self.ff(attention) + attention)  # (batch_size, seq_len, d_out)
