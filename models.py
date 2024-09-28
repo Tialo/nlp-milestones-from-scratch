0988881ade98303a353860a0a1f7619f12ffe2a4
Tialo <tialox@yandex.ru>
2024-09-28 14:00:09 +0300

    replace attn_weights  calculation with @

--- models.py
+++ models.py
@@ -14,7 +14,4 @@ class ScaledDotProductAttention(nn.Module):
         d_k = k.shape[-1]
         attn_weights = q @ k.transpose(1, 2) / d_k ** 0.5  # (batch_size, seq_len, seq_len)
         attn_weights = torch.softmax(attn_weights, dim=-1)
-        attn_weights = attn_weights[:, :, :, torch.newaxis]  # (batch_size, seq_len, seq_len, 1)
-        v = v[:, :, torch.newaxis, :]  # (batch_size, seq_len, 1, d_v)
-        attn_weights = attn_weights * v  # (batch_size, seq_len, seq_len, d_v)
-        return attn_weights.sum(dim=-2)
+        return attn_weights @ v  # (batch_size, seq_len, d_v)
