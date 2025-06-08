842788c4ff2e566136265b18ac604fcb7d0b17b8
Tialo <tialox@yandex.ru>
2025-06-08 04:30:52 +0300

    use batching in beam search

--- generator.py
+++ generator.py
@@ -15,41 +15,62 @@ class Generator:
         self.eos_token_id = eos_token_id
     
     @torch.no_grad
-    def _get_top_tokens(self, src: torch.Tensor, memory: torch.Tensor, generated: list[int], n_beams: int) -> list[tuple[int, float]]:
-        logits = self.transformer.decode(memory, torch.tensor([generated]).to(src.device))  # (1, len(generated), vocab_size)
-        logits = logits[0, -1]  # (vocab_size, )
-        token_probs = logits.log_softmax(dim=0)
-        top_tokens = torch.topk(token_probs, k=n_beams, sorted=False)
-        return list(zip(top_tokens.indices.tolist(), top_tokens.values.tolist()))
+    def _get_top_tokens(self, memory: torch.Tensor, generated: torch.Tensor, n_beams: int) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        memory - (1, src_seq_len, embed_dim)
+        generated - (batch_size, current_seq_len)
+        """
+        memory = memory.expand(generated.size(0), memory.size(1), memory.size(2))
+        logits = self.transformer.decode(memory, generated)  # (batch_size, current_seq_len, vocab_size)
+        logits = logits[:, -1]  # (batch_size, vocab_size, )
+        token_probs = logits.log_softmax(dim=-1)
+        top_tokens = torch.topk(token_probs, k=n_beams, sorted=False, dim=-1)
+        return top_tokens.indices, top_tokens.values  # each (batch_size, n_beams)
 
     @staticmethod
     def _get_normalized_score(beam: tuple[list[int], float], alpha: float = 0.6) -> float:
         # 6.1 We used beam search with a beam size of 4 and length penalty α = 0.6
-        return beam[1] * 6  ** alpha / (5 + len(beam[0])) ** alpha
+        tokens, score = beam
+        return score * 6  ** alpha / (5 + len(tokens)) ** alpha
 
-    def generate(self, src, max_tokens: int = 20, n_beams: int = 5) -> torch.Tensor:
+    def generate(self, src: torch.Tensor, max_tokens: int = 20, n_beams: int = 5) -> torch.Tensor:
         if src.dim() == 1:
             src = src.unsqueeze(0)
         elif src.size(0) != 1:
             raise ValueError("batch_size > 1 is not supported...")
+
         with torch.no_grad():
             memory, _ = self.transformer.encode(src)
         beams: list[tuple[list[int], float]] = [([self.sos_token_id], 0.0)]
 
-        for _ in range(max_tokens):
-            candidate_list = []
-            if all([generated[-1] == self.eos_token_id for generated, _ in beams]):
-                break
+        for _ in range(max_tokens):  # current_seq_len
+            candidate_beams = []
+            tokens_to_process = []
+            probabilities_to_process = []
             for generated, probability in beams:
                 if generated[-1] == self.eos_token_id:
-                    candidate_list.append((generated, probability))
-                    continue
-                for top_token, proba in self._get_top_tokens(src, memory, generated, n_beams):
-                    candidate_list.append((
-                        generated + [top_token],
-                        probability + proba,
+                    candidate_beams.append((generated, probability))
+                else:
+                    tokens_to_process.append(generated)
+                    probabilities_to_process.append(probability)
+            
+            if not tokens_to_process:
+                break
+            
+            batched_tokens_to_process = torch.tensor(tokens_to_process).to(src.device)  # (n_beams - len(finished_beams), current_seq_len)
+            top_tokens, proba = self._get_top_tokens(memory, batched_tokens_to_process, n_beams)
+            for active_beam_index in range(len(tokens_to_process)):
+                base_generated = tokens_to_process[active_beam_index]
+                base_probability = probabilities_to_process[active_beam_index]
+                for beam_index in range(n_beams):
+                    new_token = top_tokens[active_beam_index, beam_index].item()
+                    new_probabiliy = proba[active_beam_index, beam_index].item()
+                    candidate_beams.append((
+                        base_generated + [new_token],
+                        base_probability + new_probabiliy,
                     ))
-            beams = heapq.nlargest(n_beams, candidate_list, key=self._get_normalized_score)
+
+            beams = heapq.nlargest(n_beams, candidate_beams, key=self._get_normalized_score)
 
         [best_generation, _] = max(beams, key=self._get_normalized_score)
         return torch.tensor(best_generation)
